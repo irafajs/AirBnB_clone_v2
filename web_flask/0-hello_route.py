b15f459018ef6bede03714b6eaf8fe3afbf46177
irafajs <irafajs@gmail.com>
2023-12-25 19:19:39 +0200

    TASK00 A_V2 WF

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Shebang to create a python scipt"""
+
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """function to print hello wolrd at location /"""
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
